64535ceb5972bb31d46b65709ba66da418baef46
saransh-283 <saranshmm@gmail.com>
2023-02-10 15:20:01 +0530

    first commit

---
+++ src/containers/home/InterviewSettingsForm.tsx
@@ -0,0 +1,107 @@
+import { Button, Flex, Box } from "@chakra-ui/react";
+import React from "react";
+import FormSelect from "../../components/formComponents/FormSelect";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { PageNumbers } from "../../interface/home";
+import { IInterViewSettings } from "../../interface/forms";
+import {
+  interviewDurationOptions,
+  interviewLanguageOptions,
+  interviewModeOptions,
+} from "./constants";
+import { useData } from "./DataProvider";
+
+const InterviewDetailsForm: React.FC<{
+  handleTab: (n: PageNumbers) => void;
+}> = ({ handleTab }) => {
+  const {state,setState}=useData()
+  const {
+    errors,
+    touched,
+    handleSubmit,
+    values,
+    setFieldTouched,
+    setFieldValue,
+  } = useFormik<IInterViewSettings>({
+    initialValues: {
+      interviewMode: state.interviewSettings.interviewMode,
+      interviewDuration: state.interviewSettings.interviewDuration,
+      interviewLanguage: state.interviewSettings.interviewLanguage,
+    },
+    validationSchema: Yup.object().shape({
+      interviewMode: Yup.string().required("Interview Mode is required"),
+      interviewDuration: Yup.string().required("Interview Duration is required"),
+      interviewLanguage: Yup.string().required("Interview Language is required")
+    }),
+    onSubmit: (values) => {
+      console.log({ values });
+      alert("Form successfully submitted");
+    },
+  });
+
+  return (
+    <Box width="100%" as="form" onSubmit={handleSubmit as any}>
+      <Box width="100%">
+        <FormSelect
+          label="Interview Mode"
+          placeholder="Select interview mode"
+          name="interviewMode"
+          options={interviewModeOptions}
+          onChange={(field: string, value: any, shouldValidate?: boolean | undefined) => {
+            let temp = { ...state }
+            temp.interviewSettings.interviewMode = value
+            setState(temp)
+            setFieldValue(field,value,shouldValidate)
+          }}
+          onBlur={setFieldTouched}
+          value={values?.interviewMode}
+          error={errors?.interviewMode}
+          touched={touched?.interviewMode}
+        />
+        <FormSelect
+          label="Interview Duration"
+          placeholder="Select interview duration"
+          name="interviewDuration"
+          options={interviewDurationOptions}
+          onChange={(field: string, value: any, shouldValidate?: boolean | undefined) => {
+            let temp = { ...state }
+            temp.interviewSettings.interviewDuration = value
+            setState(temp)
+            setFieldValue(field,value,shouldValidate)
+          }}
+          onBlur={setFieldTouched}
+          value={values?.interviewDuration}
+          error={errors?.interviewDuration}
+          touched={touched?.interviewDuration}
+        />
+        <FormSelect
+          label="Interview Language"
+          name="interviewLanguage"
+          placeholder="Select interview language"
+          options={interviewLanguageOptions}
+          onChange={(field: string, value: any, shouldValidate?: boolean | undefined) => {
+            let temp = { ...state }
+            temp.interviewSettings.interviewLanguage = value
+            setState(temp)
+            setFieldValue(field,value,shouldValidate)
+          }}
+          onBlur={setFieldTouched}
+          error={errors.interviewLanguage}
+          touched={touched.interviewLanguage}
+          value={values.interviewLanguage}
+        />
+        <Flex w="100%" justify="flex-end" mt="4rem" gap="20px">
+          <Button colorScheme="gray" type="button" onClick={() => handleTab(1)}>
+            Previous
+          </Button>
+          <Button colorScheme="red" type="submit">
+            Submit
+          </Button>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
+
+export default InterviewDetailsForm;
